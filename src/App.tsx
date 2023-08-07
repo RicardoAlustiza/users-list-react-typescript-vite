320c9a3e65ce45ee40563ebf681c5743c1a00bb9
Ricardo Alustiza <ricardo.alustiza@selectra.com>
2023-08-07 16:03:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useMemo, useRef, useState } from 'react'
+import './App.css'
+import { SortBy, type User } from './types.d'
+import { UsersListComponent } from './components/UsersListComponent'
+
+function App () {
+  const [users, setUsers] = useState<User[]>([])
+  const [showColors, setShowColors] = useState(false)
+  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
+  const [filterCountry, setFilterCountry] = useState<string | null>(null)
+  const originalUsers = useRef<User[]>([]) /* Guardar un valor que se comparte entre renders pero que al cambiar, no vuelva a rednerizar el componente */
+
+  const toggleColors = () => {
+    setShowColors(!showColors)
+  }
+
+  const toggleSortByCountry = () => {
+    const newsortingValue = sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
+    setSorting(newsortingValue)
+  }
+
+  const handleDelete = (uuid: string) => {
+    const filteredUsers = users.filter((user) => user.login.uuid !== uuid)
+    setUsers(filteredUsers)
+  }
+
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  }
+
+  const handleChangeSort = (sort: SortBy) => {
+    setSorting(sort)
+  }
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+        originalUsers.current = res.results
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+
+  const filteredUsers = useMemo(() => {
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter((user) => {
+        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
+      })
+      : users
+  }, [users, filterCountry])
+
+  const sortedUsers = useMemo(() => {
+    if (sorting === SortBy.NONE) return filteredUsers
+
+    const compareProperties: Record<string, (user: User) => any> = {
+      [SortBy.COUNTRY]: user => user.location.country,
+      [SortBy.NAME]: user => user.name.first,
+      [SortBy.LAST]: user => user.name.last
+    }
+
+    return filteredUsers.toSorted((a, b) => {
+      const extractProperty = compareProperties[sorting]
+      return extractProperty(a).localeCompare(extractProperty(b))
+    })
+  }, [filteredUsers, sorting])
+
+  return (
+    <div className='App'>
+      <header>
+        <button onClick={toggleColors}>Colorear filas</button>
+        <button onClick={toggleSortByCountry}>{sorting === SortBy.COUNTRY ? 'No ordenar por país' : 'Ordenar por país'}</button>
+        <button onClick={handleReset}>Restaurar usuarios</button>
+        <input placeholder='Filtrar por país' onChange={(e) => {
+          setFilterCountry(e.target.value)
+        }}/>
+      </header>
+      <main>
+        <UsersListComponent users={sortedUsers} showColors={showColors} deleteUser={handleDelete} changeSorting={handleChangeSort}/>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/UsersListComponent.tsx
@@ -0,0 +1,53 @@
+import { SortBy, type User } from '../types.d'
+
+interface Props {
+  users: User[]
+  showColors: boolean
+  deleteUser: (uuid: string) => void
+  changeSorting: (sort: SortBy) => void
+}
+
+export function UsersListComponent ({ users, showColors, deleteUser, changeSorting }: Props) {
+  return (
+    <table width='100%'>
+        <thead>
+            <tr>
+                <th>Foto</th>
+                <th className="pointer" onClick={() => { changeSorting(SortBy.NAME) }}>Nombre</th>
+                <th className="pointer" onClick={() => { changeSorting(SortBy.LAST) }}>Apellido</th>
+                <th className="pointer" onClick={() => { changeSorting(SortBy.COUNTRY) }}>País</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {
+                users.map((user, index) => {
+                  const backgroundColor = index % 2 === 0 ? '#333' : '#555'
+                  const color = showColors ? backgroundColor : 'transparent'
+
+                  return (
+                    <tr key={user.login.uuid} style={{ backgroundColor: color }}>
+                        <td>
+                            <img src={user.picture.thumbnail} />
+                        </td>
+                        <td>
+                          {user.name.first}
+                        </td>
+                        <td>
+                          {user.name.last}
+                        </td>
+                        <td>
+                          {user.location.country}
+                        </td>
+                        <td>
+                          <button onClick={() => { deleteUser(user.login.uuid) }}>Eliminar</button>
+                        </td>
+                    </tr>
+                  )
+                })
+            }
+        </tbody>
+    </table>
+  )
+}
